8705537b0c4b8ee7e89c7b1bbb0aafb1c8699397
kiririmode <kiririmode@gmail.com>
2023-06-11 16:40:51 +0900

    fix: refactoring

--- src/main/java/com/kiririmode/OsmMapFrame.java
+++ src/main/java/com/kiririmode/OsmMapFrame.java
@@ -7,21 +7,18 @@ import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.MapContent;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
-import org.geotools.styling.Stroke;
 import org.geotools.styling.*;
 import org.geotools.swing.JMapFrame;
 import org.geotools.tile.TileService;
 import org.geotools.tile.impl.osm.OSMService;
 import org.geotools.tile.util.TileLayer;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.LineString;
-import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.*;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
 import java.awt.*;
 import java.util.Arrays;
+import java.util.function.Function;
 
 public class OsmMapFrame {
     public static void main(String args[]) throws SchemaException {
@@ -37,10 +34,14 @@ public class OsmMapFrame {
                 new Coordinate(141.356882, 43.068624)     // さっぽろテレビ塔
         };
 
-        FeatureLayer lineLayer = createLineLayer(coordinates, 2.0);
+        FeatureLayer lineLayer = createLineLayer("Line", "geometry:LineString", coordinates,
+                styleBuilder -> styleBuilder.createLineSymbolizer(styleBuilder.createStroke(Color.BLUE, 2.0)));
         map.addLayer(lineLayer);
 
-        FeatureLayer pointLayer = createPointLayer(coordinates);
+        FeatureLayer pointLayer = createPointLayer("Point", "geometry:Point", coordinates,
+                styleBuilder -> styleBuilder.createPointSymbolizer(
+                        styleBuilder.createGraphic(null,
+                                styleBuilder.createMark(StyleBuilder.MARK_CIRCLE, new Color(0,0,0,0), Color.BLUE, 1.0), null, 1,10, 0)));
         map.addLayer(pointLayer);
 
         setBound(map, coordinates);
@@ -53,63 +54,41 @@ public class OsmMapFrame {
         double minLon = Arrays.stream(coordinates).mapToDouble(c -> c.x).min().orElseThrow(IllegalStateException::new);
         double maxLon = Arrays.stream(coordinates).mapToDouble(c -> c.x).max().orElseThrow(IllegalStateException::new);
 
-        // 表示範囲を設定
         double offset = 0.001;
-        ReferencedEnvelope envelope = new ReferencedEnvelope(minLon - offset, maxLon + offset, minLat - offset, maxLat + offset, DefaultGeographicCRS.WGS84);
-
-        // MapContentに表示範囲を設定
-        map.getViewport().setBounds(envelope);
+        map.getViewport().setBounds(new ReferencedEnvelope(minLon - offset, maxLon + offset, minLat - offset, maxLat + offset, DefaultGeographicCRS.WGS84));
     }
 
-    private static FeatureLayer createLineLayer(Coordinate[] coordinates, double lineWidth) throws SchemaException {
-        GeometryFactory geometryFactory = new GeometryFactory();
+    private static FeatureLayer createLineLayer(String typeName, String typeSpec, Coordinate[] coordinates, Function<StyleBuilder, Symbolizer> symbolizerCreator) throws SchemaException {
+        SimpleFeatureType type = DataUtilities.createType(typeName, typeSpec);
+        DefaultFeatureCollection featureCollection = new DefaultFeatureCollection();
 
-        // Lineという名前で「線」のFeatureType、Featureを作成
-        SimpleFeatureType lineStringType = DataUtilities.createType("Line", "geometry:LineString");
-        SimpleFeature lineStringFeature = DataUtilities.template(lineStringType);
-
-        // getmetryの指定
-        LineString lineString = geometryFactory.createLineString(coordinates);
-        lineStringFeature.setDefaultGeometry(lineString);
-
-        StyleBuilder styleBuilder = new StyleBuilder();
-        // 線のスタイル作成
-        Stroke stroke = styleBuilder.createStroke(Color.BLUE, lineWidth);
-        LineSymbolizer symbolizer = styleBuilder.createLineSymbolizer(stroke);
-
-        Rule rule = styleBuilder.createRule(symbolizer);
-        FeatureTypeStyle fts = styleBuilder.createFeatureTypeStyle("Feature", new Rule[]{rule});
-
-        Style style = styleBuilder.createStyle();
-        style.featureTypeStyles().add(fts);
-
-        return new FeatureLayer(DataUtilities.collection(lineStringFeature), style);
+        SimpleFeature feature = DataUtilities.template(type);
+        feature.setDefaultGeometry(new GeometryFactory().createLineString(coordinates));
+        featureCollection.add(feature);
+        return getFeatureLayer(typeName, symbolizerCreator, featureCollection);
     }
 
-    private static FeatureLayer createPointLayer(Coordinate[] coordinates) throws SchemaException {
-        GeometryFactory geometryFactory = new GeometryFactory();
 
-        SimpleFeatureType pointType = DataUtilities.createType("Point", "geometry:Point");
-        DefaultFeatureCollection points = new DefaultFeatureCollection();
+    private static FeatureLayer createPointLayer(String typeName, String typeSpec, Coordinate[] coordinates, Function<StyleBuilder, Symbolizer> symbolizerCreator) throws SchemaException {
+        SimpleFeatureType type = DataUtilities.createType(typeName, typeSpec);
+        DefaultFeatureCollection featureCollection = new DefaultFeatureCollection();
 
-        for (Coordinate c : coordinates) {
-            Point point = geometryFactory.createPoint(c);
-            SimpleFeature pointFeature = DataUtilities.template(pointType);
-            pointFeature.setDefaultGeometry(point);
-            points.add(pointFeature);
+        for (Coordinate coordinate : coordinates) {
+            SimpleFeature feature = DataUtilities.template(type);
+            feature.setDefaultGeometry(new GeometryFactory().createPoint(coordinate));
+            featureCollection.add(feature);
         }
 
+        return getFeatureLayer(typeName, symbolizerCreator, featureCollection);
+    }
+
+    private static FeatureLayer getFeatureLayer(String typeName, Function<StyleBuilder, Symbolizer> symbolizerCreator, DefaultFeatureCollection featureCollection) {
         StyleBuilder styleBuilder = new StyleBuilder();
-
-        Mark mark = styleBuilder.createMark(StyleBuilder.MARK_CIRCLE, new Color(0,0,0,0), Color.BLUE, 1.0);
-        Graphic graphic = styleBuilder.createGraphic(null, mark, null, 1,10, 0);
-        PointSymbolizer pointSymbolizer = styleBuilder.createPointSymbolizer(graphic);
-
-        Rule rule = styleBuilder.createRule(pointSymbolizer);
-        FeatureTypeStyle fts = styleBuilder.createFeatureTypeStyle("Feature", new Rule[]{rule});
+        Rule rule = styleBuilder.createRule(symbolizerCreator.apply(styleBuilder));
+        FeatureTypeStyle fts = styleBuilder.createFeatureTypeStyle(typeName, rule);
         Style style = styleBuilder.createStyle();
         style.featureTypeStyles().add(fts);
 
-        return new FeatureLayer(points, style);
+        return new FeatureLayer(featureCollection, style);
     }
 }
